5528dd5a8095facad031667e28eba0ce890161d9
rico2003 <rickeyswari2003@gmail.com>
2019-10-28 22:06:34 +0000

    I am so confuse

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+${"button"}.click(function(){
+    $(".output").text("");
+ var words =$("input").val();
+ var words = 0;
+ nums_array.forEach(function(element){
+     words = element;
+ });
+$('.output').append(nums"="+sums);
 
-console.log("rickeyiscool");
-
-
-
-
-
+});
 });
